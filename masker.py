dde8ca790fef172ebfc64e9c677c90ec644f9999
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2022-01-15 14:49:23 +0700

    Prototype 1

---
+++ masker.py
@@ -0,0 +1,43 @@
+import cv2
+
+facecade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+nosecade = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
+
+video_capture = cv2.VideoCapture(0)
+mask_on = False
+
+while True:
+    #ambil frame by frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    wajah = facecade.detectMultiScale(gray, 1.1, 5)
+
+    #gambar kotak di wajah
+    for(x, y, w, h) in wajah:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        if mask_on:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            cv2.putText(frame, 'Mask on', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 5)
+        else:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
+            cv2.putText(frame, 'Mask off', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+
+        hidung = nosecade.detectMultiScale(roi_gray, 1.18, 35)
+        for (sx, sy, sw, sh) in hidung:
+            cv2.rectangle(roi_color, (sh, sy), (sx+sw, sy+sh), (255, 0, 0), 2)
+            cv2.putText(frame, 'hidung', (x + sx,y + sy), 1, 1, (0, 255, 0), 1)
+            
+            if len(hidung) == 0:
+                mask_on = True
+            elif len(hidung) > 0:
+                mask_on = False
+
+    cv2.putText(frame, 'jumlah wajah: ' + str(len(wajah)), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ miku.py
@@ -0,0 +1,118 @@
+import pyttsx3 #pip install di terminal
+import speech_recognition as sr
+import datetime
+import wikipedia
+import pywhatkit
+import webbrowser
+import os
+import smtplib
+
+print ("initializing Kaito")
+
+MASTER = "ilyas"
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[1].id)
+
+#speak
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#function
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour >= 0 and hour < 12:
+        speak("Good Morning master" + MASTER)
+    elif hour >= 12 and hour < 18:
+        speak("Good Aternoon master" + MASTER)
+    else:
+        speak("Good Evening master" + MASTER)
+        speak("")
+
+#microphone
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+        try:
+            print("Recognition...")
+            query = r.recognize_google(audio, language="en-us")
+            print(f"user said: {query}\n")
+
+        except Exception as e:
+            print("say that again please")
+            speak("say that again please")
+            query = None
+    return query
+
+#main start here
+wishMe()
+speak("Hello my name is Kaito, Can I help you master?")
+query = takeCommand()
+
+#logic for tasks as command
+INTRO = ["please introduce you","who are you"]
+if query.lower() in INTRO:
+    speak("My name is Kaito. I am the beta version of Miku. I was created in 2021 and my creator is named Ilyas Hidayat Rusdy. nice to meet you.")
+elif "wikipedia" in query.lower():
+    speak("ok master, searching wikipedia...")
+    query = query.replace("wikipedia", "")
+    results = wikipedia.summary(query, sentences=2)
+    print(results)
+    speak(results)
+elif "open youtube" in query.lower():
+    speak("Ok, master")
+    url = "youtube.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "google" in query.lower():
+    speak("Ok master, searching google...")
+    pywhatkit.search(query.replace("google", ""))
+elif "youtube" in query.lower():
+    speak("Ok master, searching youtube...")
+    pywhatkit.playonyt(query.replace("youtube", ""))
+elif "time" in query.lower():
+    time = datetime.datetime.now().strftime("%I:%M %p")
+    print(time)
+    speak("time now is" + time)
+elif "open whatsapp" in query.lower():
+    speak("Ok, master")
+    url = "web.whatsapp.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "google meet" in query.lower():
+    speak("Ok, master")
+    url = "meet.google.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "open mail" in query.lower():
+    speak("Ok, master")
+    url = "mail.google.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "open drive" in query.lower():
+    speak("Ok, master")
+    url = "drive.google.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "open google" in query.lower():
+    speak("Ok, master")
+    url = "google.com"
+    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(chrome_path).open(url)
+elif "mask" in query.lower():
+    speak("Ok, master")
+    import masker
+elif "play music" in query.lower():
+    speak("Ok, master")
+    songs_dir = "C:\\Users\\Ilyas Hidayat Rusdy\\Music"
+    songs = os.listdir(songs_dir)
+    print(songs)
+    os.startfile(os.path.join(songs_dir, songs[0]))
+else:
+    speak("sorry master, your order is not including my program")
