56b55b835839b9f53dd5dfbdbf16ef1ab1484d28
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2022-12-08 10:42:16 +0700

    Update Model

--- traningface.py
+++ traningface.py
@@ -25,7 +25,11 @@ def TrainingFace():
     faces, ids = getImagesWithLabels('dataset')
     recognizer.train(faces, np.array(ids))
     recognizer.write('dataset/trainer.yml')
-    print("{0} faces trained. Exiting Program".format(len(np.unique(ids))))
+    # Accurate face recognition training treshold
+    Accuracy = round(recognizer.getThreshold(), 2)
+    print("\n Accuracy: {0}%".format(Accuracy))
+    # Print the numer of faces trained and end program
+    print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 
 
 # #main
